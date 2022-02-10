ebb300b85781a296c93acc29a77a2869cb40db82
Wenti-D <windy0deng@gmail.com>
2022-02-10 20:46:20 +0800

    Minor fixes.

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # 《你缺失的那门计算机课》PDF 版本
 
-这是《你缺失的那门计算机课》的 PDF 版本 LaTeX 源代码。使用
+这是《你缺失的那门计算机课》的 PDF 版本 LaTeX 源代码。重复执行两遍
 
 ```
-latexmk -xelatex
+xelatex -output-driver="xdvipdfmx -i dvipdfmx-unsafe.cfg -q -E" missing.tex
 ```
 
-来生成 PDF 文件。
+或者直接双击 `make.bat` 来生成 PDF 文件。 
---
+++ make.bat
@@ -0,0 +1,13 @@
+@echo off
+cd /d %~dp0
+set TEMPFILE=missing.aux missing.log missing.out missing.toc missing.xdv missing.synctex.gz
+set OPT=-interaction=nonstopmode -output-driver="xdvipdfmx -i dvipdfmx-unsafe.cfg -q -E"
+
+xelatex %OPT% --no-pdf missing.tex
+xelatex %OPT% missing.tex
+
+del %TEMPFILE%
+
+if exist missing.pdf (start missing.pdf)
+
+exit

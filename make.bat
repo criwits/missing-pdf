f3d11a025b0faecbd1762405ddac1d5f1732162d
criwits <hanswan@tom.com>
2022-02-10 21:27:59 +0800

    Fix: make script

--- make.bat
+++ make.bat
@@ -1,6 +1,6 @@
 @echo off
 cd /d %~dp0
-set TEMPFILE=missing.aux missing.log missing.out missing.toc missing.xdv missing.synctex.gz
+set TEMPFILE=missing.aux chapters\*.aux missing.log missing.out missing.toc missing.xdv missing.synctex.gz
 set OPT=-interaction=nonstopmode -output-driver="xdvipdfmx -i dvipdfmx-unsafe.cfg -q -E"
 
 xelatex %OPT% --no-pdf missing.tex

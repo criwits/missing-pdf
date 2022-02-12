61f6fad04d19bfcc47e8a14109ad5c4cce007ebd
Wenti-D <windy0deng@gmail.com>
2022-02-13 02:02:09 +0800

    Optimize title styles. Fix compiling problems.

--- make.bat
+++ make.bat
@@ -1,8 +1,9 @@
 @echo off
 cd /d %~dp0
 set TEMPFILE=missing.aux chapters\*.aux missing.log missing.out missing.toc missing.xdv missing.synctex.gz
-set OPT=-interaction=nonstopmode -output-driver="xdvipdfmx -i dvipdfmx-unsafe.cfg -q -E"
+set OPT=-interaction=nonstopmode -synctex=1 -output-driver="xdvipdfmx -i dvipdfmx-unsafe.cfg -q -E"
 
+xelatex --no-pdf missing.tex
 xelatex %OPT% --no-pdf missing.tex
 xelatex %OPT% missing.tex
 

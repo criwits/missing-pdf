299fd755f60590d3c50c4e9c5586f530c7a1764b
criwits <hanswan@tom.com>
2022-02-13 13:06:23 +0800

    Changed some expressions.

--- make.bat
+++ make.bat
@@ -1,7 +1,7 @@
 @echo off
 cd /d %~dp0
 set TEMPFILE=missing.aux chapters\*.aux missing.log missing.out missing.toc missing.xdv missing.synctex.gz
-set OPT=-interaction=nonstopmode -synctex=1 -output-driver="xdvipdfmx -i dvipdfmx-unsafe.cfg -q -E"
+set OPT=-interaction=nonstopmode -synctex=1 -output-driver="xdvipdfmx -i dvipdfmx-unsafe.cfg -q -E" --shell-escape
 
 xelatex --no-pdf missing.tex
 xelatex %OPT% --no-pdf missing.tex
